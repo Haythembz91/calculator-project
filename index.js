538ff81fe72c732de21308a704769ea436ad242e
Haythem91 <haylive8@gmail.com>
2024-04-02 08:22:23 +0100

    'second commit'

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let operation = document.querySelector('.operation')
 let operator = document.querySelectorAll('.operator')
 let cancel = document.querySelector('.cancel')
 let equalKey = document.querySelector('.equalKey')
+let overwrite = false
 
 let sqr = document.querySelector('.sqr')
 sqr.addEventListener('click',()=>result.innerHTML=Math.pow(parseFloat(result.innerHTML),2))
@@ -15,6 +16,8 @@ sqrt.addEventListener('click',()=>result.innerHTML=Math.sqrt(parseFloat(result.i
 
 let mathPi = document.querySelector('.math-pi')
 mathPi.addEventListener('click',() => result.innerHTML = Math.PI.toFixed(10))
+
+
 
 cancel.addEventListener('click',clear)
 function clear(){
@@ -27,13 +30,19 @@ function clear(){
 for (let i=0; i<number.length ; i++){
     number[i].addEventListener('click',display)
         function display (){
-        result.innerHTML += number[i].textContent
-            console.log(result.innerHTML.includes('.'))
-            if (result.innerHTML.includes('.')){
-                number[10].removeEventListener('click',display)
+            if(overwrite){
+                result.innerHTML=''
+                overwrite=false
             }
+            result.innerHTML += number[i].textContent
+                
+                if (result.innerHTML.includes('.')){
+                    number[10].removeEventListener('click',display)
+                }
+            
         }
 }
+
 
 
 for (let i=0;i<operator.length;i++){
@@ -78,6 +87,8 @@ function compute(){
 equalKey.addEventListener('click',equal)
 
 function equal (){
+    overwrite = true
+    console.log(overwrite)
     compute()
     operation.innerHTML = ''
     if (result.innerHTML.includes('.')){
